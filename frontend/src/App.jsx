467ab8d0844914117cd53ba173e49aef29fb9b12
akhoa79 <khoanda5875@ut.edu.vn>
2026-08-19 14:40:49 +0700

    feat(UI/UX): Giao diện trang giới thiệu

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TrangGioiThieu from './TrangGioiThieu.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <TrangGioiThieu />
+    </div>
+  )
+}
+
+export default App
